e3cf77dfdb03dfc44830a9e55a0870dfacf7fe2f
Angela Hanshaw <angela.hanshaw@gmail.com>
2021-07-10 10:50:52 -0500

    fix broken post link

--- src/components/PostsItem/PostsItem.js
+++ src/components/PostsItem/PostsItem.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 export function PostsItem({post}) {
 	return (
 		<div className="posts-item">
-			<h2><Link to={`post/${post.id}`}>{post.title}</Link></h2>
+			<h2><Link to={`/post/${post.id}`}>{post.title}</Link></h2>
 			<p>{post.body}</p>
 		</div>
     );
